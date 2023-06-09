89aeacbddba5423a8f5059bb48f1a05774baf655
yatomiyoshi <seno.yatomi.0407@gmail.com>
2023-06-09 21:00:30 +0900

    start practice-react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,9 +21,65 @@ function App() {
         >
           Learn React
         </a>
+        <p>Learn SPA</p>
+        <div>
+          <h1>Welcom to my app</h1>
+          <MyButton />
+        </div>
+        <div>
+          <h2>Welcome to my app button2</h2>
+          <MyButton2 />
+        </div>
+        <div>
+          <AboutPage />
+          <Profile />
+        </div>
       </header>
     </div>
   );
 }
 
+function MyButton() {
+  return <button>I'm a button</button>;
+}
+
+function MyButton2() {
+  return <button color="blue">button2</button>;
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h3>About</h3>
+      <p>
+        Hello there.
+        <br />
+        How do you do?
+      </p>
+    </>
+  );
+}
+
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </>
+  );
+}
 export default App;
